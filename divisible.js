b36a1d4136095d6357788daf43f16fcd92bce464
barifahim007 <ehsanulbarifhahim7@gmail.com>
2022-11-09 00:51:08 +0600

    divisible

---
+++ divisible.js
@@ -0,0 +1,15 @@
+// write foo, bar if the number divisble by 3 and 5 both
+for (let i = 1; i <= 50; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log('foobar')
+    }
+    else if (i % 3 === 0) {
+        console.log('foo')
+    }
+    else if (i % 5 === 0) {
+        console.log('bar')
+    }
+    else {
+        console.log(i)
+    }
+}
